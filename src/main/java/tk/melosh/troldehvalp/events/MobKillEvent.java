c6d1c96e0a250ac03fd8b9a8506035b1eb371d5a
melosh <milasholsting@protonmail.com>
2022-06-17 07:49:06 +0200

    update MobKillEvent

--- src/main/java/tk/melosh/troldehvalp/events/MobKillEvent.java
+++ src/main/java/tk/melosh/troldehvalp/events/MobKillEvent.java
@@ -1,5 +1,6 @@
 package tk.melosh.troldehvalp.events;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -22,16 +23,21 @@ public class MobKillEvent implements Listener {
 
     @EventHandler
     public void onEntityDeath(EntityDeathEvent event) {
+        plugin.LOGGER.info("inside event");
         LivingEntity target = event.getEntity();
         LivingEntity killer;
+        plugin.LOGGER.info(target.getType().name());
         if(target.getType() != EntityType.COW)
             return;
 
-        if(target.getKiller().getType() != EntityType.PLAYER)
+        if(target.getKiller().getType() != EntityType.PLAYER) {
+            plugin.LOGGER.info("killer isnt a player");
             return;
+        }
 
         killer = target.getKiller().getPlayer();
         if(killer == null) {
+            plugin.LOGGER.severe("killer is null");
             return;
         }
         try {
@@ -40,10 +46,16 @@ public class MobKillEvent implements Listener {
                 player = new PlayerModel(killer.getUniqueId(), killer.getName(), 100);
             }
             player.money += 10;
-            System.out.print(player.money);
-            player.save();
+            plugin.LOGGER.info(String.format("player money = %s", player.money));
+            if(player.save()) {
+                plugin.LOGGER.info("player saved");
+                PlayerModel p = PlayerModel.getPlayer(player.uuid);
+                plugin.LOGGER.info(String.valueOf(p.money));
+            }else {
+                plugin.LOGGER.info("player not saved");
+            }
         } catch (SQLException e) {
-            System.out.print(e.getMessage());
+            plugin.LOGGER.severe(e.getMessage());
         }
     }
 }
